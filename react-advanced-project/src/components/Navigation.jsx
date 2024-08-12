dea33ef3425bbcfaf4b74d42568d9758cb9e4dde
ShannahvdWal <shannahvdwal@gmail.com>
2024-08-12 16:22:19 +0200

    Created AddEventPage, updated navigation

--- react-advanced-project/src/components/Navigation.jsx
+++ react-advanced-project/src/components/Navigation.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Events</Link>
-        </li>
-        <li>
-          <Link to="/event/1">Event</Link>
-        </li>
+        <Button>
+          <li>
+            <Link to="/">Events</Link>
+          </li>
+        </Button>
+        <Button>
+          <li>
+            <Link to="/event/1">Event</Link>
+          </li>
+        </Button>
+        <Button>
+          <li>
+            <Link to="/add-event">Add Event</Link>
+          </li>
+        </Button>
       </ul>
     </nav>
   );
---
+++ react-advanced-project/src/pages/AddEventPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { Form } from "react-router-dom";
+
+export const AddEventPage = () => {
+  return (
+    <div className="add-event">
+      <Heading>Add New Event</Heading>
+      <Form></Form>
+    </div>
+  );
+};
