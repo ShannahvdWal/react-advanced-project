9a29890101378f8c4b33bdd77023eeb95efdd629
ShannahvdWal <shannahvdwal@gmail.com>
2024-08-12 16:09:57 +0200

    Basic event list styling, clickable event list items

--- react-advanced-project/src/pages/EventsPage.jsx
+++ react-advanced-project/src/pages/EventsPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Heading } from "@chakra-ui/react";
-import { useLoaderData } from "react-router-dom";
+import React from "react";
+import { Heading, Card, CardBody, SimpleGrid } from "@chakra-ui/react";
+import { Link, useLoaderData } from "react-router-dom";
 
 export const loader = async ({ params }) => {
   const events = await fetch("http://localhost:3000/events");
@@ -17,19 +17,39 @@ export const EventsPage = () => {
 
   return (
     <div className="event-list">
-      <Heading>List of events</Heading>
+      <Heading size="xl">List of events</Heading>
       <ul>
-        {events &&
-          events.map((event) => (
-            <li key={event.id}>
-              <b>{event.title}</b>
-              <p>{event.description}</p>
-              <img src={event.image}></img>
-              <p>{event.startTime}</p>
-              <p>{event.endTime}</p>
-              <p>{users.find((user) => event.createdBy === user.id).name}</p>
-            </li>
-          ))}
+        <SimpleGrid
+          spacing={4}
+          templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+        >
+          {events &&
+            events.map((event) => (
+              <Card maxW="sm">
+                <Link to={`/event/${event.id}`}>
+                  <li key={event.id}>
+                    <CardBody>
+                      <Heading size="md">
+                        <b>{event.title}</b>
+                      </Heading>
+                      <p>{event.description}</p>
+                      <img src={event.image}></img>
+                      <p>
+                        <b>Start:</b> {event.startTime}
+                      </p>
+                      <p>
+                        <b>End:</b> {event.endTime}
+                      </p>
+                      <p>
+                        <b>Added by: </b>
+                        {users.find((user) => event.createdBy === user.id).name}
+                      </p>
+                    </CardBody>
+                  </li>
+                </Link>
+              </Card>
+            ))}
+        </SimpleGrid>
       </ul>
     </div>
   );
