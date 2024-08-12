64416afd2f9e9908fbe63f2652099edf3acc2b38
ShannahvdWal <shannahvdwal@gmail.com>
2024-08-12 13:32:11 +0200

    Fetched events from server, listed events in list

--- react-advanced-project/src/pages/EventsPage.jsx
+++ react-advanced-project/src/pages/EventsPage.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React, { useEffect, useState } from "react";
+import { Heading } from "@chakra-ui/react";
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch("http://localhost:3000/events");
+      const event = await response.json();
+      setEvents(event);
+    };
+    fetchEvents();
+  }, []);
+
+  return (
+    <div className="event-list">
+      <Heading>List of events</Heading>
+      <ul>
+        {events &&
+          events.map((event) => (
+            <li key={event.id}>
+              <b>{event.title}</b>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
