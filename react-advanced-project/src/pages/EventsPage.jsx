7f3cb576354f7824b20bb99f3cb312b54d5f6d5a
ShannahvdWal <shannahvdwal@gmail.com>
2024-08-16 15:36:29 +0200

    Added search function to EventsPage

--- react-advanced-project/src/pages/EventsPage.jsx
+++ react-advanced-project/src/pages/EventsPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Heading,
   Card,
@@ -27,13 +27,18 @@ export const loader = async ({ params }) => {
 
 export const EventsPage = () => {
   const { events, categories } = useLoaderData();
+  const [query, setQuery] = useState(" ");
 
   return (
     <div className="event-list">
       <Heading className="heading-large">
-        List of events
+        Upcoming Events
         <InputGroup>
-          <Input placeholder="Search for events..."></Input>
+          <Input
+            onChange={(event) => setQuery(event.target.value)}
+            setQuery={setQuery}
+            placeholder="Search for events..."
+          ></Input>
           <InputRightAddon>
             <SearchIcon />
           </InputRightAddon>
@@ -46,40 +51,46 @@ export const EventsPage = () => {
           templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
         >
           {events &&
-            events.map((event) => (
-              <Card maxW="sm">
-                <Link to={`/event/${event.id}`}>
-                  <li key={event.id}>
-                    <CardBody>
-                      <Heading size="md">
-                        <b>{event.title}</b>
-                      </Heading>
-                      <p>{event.description}</p>
-                      <img src={event.image}></img>
-                      <p>
-                        <b>Start: </b>
-                        {event.startTime}
-                      </p>
-                      <p>
-                        <b>End: </b> {event.endTime}
-                      </p>
-                      <p>
-                        <b>Categories: </b>
-                        {categories
-                          .filter((category) =>
-                            event.categoryIds.includes(category.id)
-                          )
-                          .map((category) => (
-                            <Tag marginEnd={2} key={category.id}>
-                              {category.name}
-                            </Tag>
-                          ))}
-                      </p>
-                    </CardBody>
-                  </li>
-                </Link>
-              </Card>
-            ))}
+            events
+              .filter((event) => {
+                return query.toLowerCase() === " "
+                  ? event
+                  : event.title.toLowerCase().includes(query);
+              })
+              .map((event) => (
+                <Card maxW="sm">
+                  <Link to={`/event/${event.id}`}>
+                    <li key={event.id}>
+                      <CardBody>
+                        <Heading size="md">
+                          <b>{event.title}</b>
+                        </Heading>
+                        <p>{event.description}</p>
+                        <img src={event.image}></img>
+                        <p>
+                          <b>Start: </b>
+                          {event.startTime}
+                        </p>
+                        <p>
+                          <b>End: </b> {event.endTime}
+                        </p>
+                        <p>
+                          <b>Categories: </b>
+                          {categories
+                            .filter((category) =>
+                              event.categoryIds.includes(category.id)
+                            )
+                            .map((category) => (
+                              <Tag marginEnd={2} key={category.id}>
+                                {category.name}
+                              </Tag>
+                            ))}
+                        </p>
+                      </CardBody>
+                    </li>
+                  </Link>
+                </Card>
+              ))}
         </SimpleGrid>
       </ul>
     </div>
